fb090ea28124d620a69e4f44c473903cb85fe1ad
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-03-04 14:04:06 +0300

    Assignment done

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
 class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
     pass
+
+
+
+user = User('Juma', 'TheQue')
+print(user.first_name)
+print(user.last_name)
